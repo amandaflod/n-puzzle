5c44fa2ded25eb4db6784131b43bf502388b4a17
amandaflod <amandaflodstroem@gmail.com>
2023-03-22 15:46:14 +0100

    Attempt to create handleMove function

--- src/Puzzle.tsx
+++ src/Puzzle.tsx
@@ -20,7 +20,8 @@ export const Puzzle = () => {
   const [tiles, setTiles] = useState<number[]>([]);
 
   const shuffle = () => {
-    const numbers = [...Array(rows * columns).keys()];
+    const numbers = [...Array(rows * columns - 1).keys()].map((n) => n + 1);
+    numbers.push(-1);
 
     for (let i = numbers.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
@@ -33,11 +34,34 @@ export const Puzzle = () => {
     shuffle();
   }, []);
 
+  const handleMove = (position: number) => {
+    const emptyIndex = tiles.indexOf(-1);
+    const emptyRow = Math.floor(emptyIndex / columns);
+    const emptyCol = emptyIndex % columns;
+
+    const row = Math.floor(position / columns);
+    const col = position % columns;
+
+    if (
+      (row === emptyRow && Math.abs(col - emptyCol) === 1) ||
+      (col === emptyCol && Math.abs(row - emptyRow) === 1)
+    ) {
+      const newTiles = [...tiles];
+      const indexDiff = position - emptyIndex;
+      const step = indexDiff > 0 ? 1 : -1;
+      for (let i = emptyIndex; i !== position; i += step) {
+        newTiles[i] = newTiles[i + step];
+      }
+      newTiles[position] = -1;
+      setTiles(newTiles);
+    }
+  };
+
   return (
     <div>
       <PuzzleWrapper columns={columns} rows={rows}>
-        {tiles.map((number) => (
-          <Tile number={number} />
+        {tiles.map((number, index) => (
+          <Tile number={number} position={index} onMove={handleMove} />
         ))}
       </PuzzleWrapper>
       <button onClick={shuffle}>Shuffle</button>
--- src/Tile.tsx
+++ src/Tile.tsx
@@ -2,12 +2,18 @@ import styled from "styled-components";
 
 type TileProps = {
   number: number;
+  position: number;
+  onMove: (position: number) => void;
 };
 
 const TileWrapper = styled.div`
   border: 1px solid black;
 `;
 
-export const Tile = ({ number }: TileProps) => {
-  return <TileWrapper>{number}</TileWrapper>;
+export const Tile = ({ number, onMove, position }: TileProps) => {
+  return (
+    <TileWrapper onClick={() => onMove(position)}>
+      {number !== -1 ? number : ""}
+    </TileWrapper>
+  );
 };
