d238be89e7b21a01d7dcd93ba20a4e7126aa6262
amandaflod <amandaflodstroem@gmail.com>
2023-03-23 19:58:40 +0100

    Add styling and make it work

--- src/Puzzle.tsx
+++ src/Puzzle.tsx
@@ -1,5 +1,5 @@
-import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { useState } from "react";
 import { Tile } from "./Tile";
 
 type PuzzleProps = {
@@ -7,40 +7,188 @@ type PuzzleProps = {
   columns: number;
 };
 
-const PuzzleWrapper = styled.div<PuzzleProps>`
+const PuzzleWrapper = styled.div`
+  max-width: 500px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  padding: 3rem;
+`;
+
+const StyledPuzzle = styled.div<PuzzleProps>`
+  width: 100%;
   display: grid;
   grid-template-columns: repeat(${({ columns }) => columns}, 1fr);
   grid-template-rows: repeat(${({ rows }) => rows}, 1fr);
+  gap: 0.5rem;
 `;
 
-export const Puzzle = () => {
-  const rows = 4;
-  const columns = 4;
+const Button = styled.button`
+  border: none;
+  cursor: pointer;
+  margin-top: 1rem;
+  border-radius: 0.5rem;
+  padding: 0.5rem 1rem;
+  color: white;
+  background-color: black;
+  font-family: inherit;
+  font-size: 1rem;
+  box-shadow: 0 0.5rem 1rem lightgray;
+  &:hover {
+    background-color: dimgray;
+  }
+  &:active {
+    transform: translateY(2px);
+  }
+`;
 
-  const [tiles, setTiles] = useState<number[]>([]);
+const rows = 5;
+const columns = 5;
 
-  const shuffle = () => {
-    const numbers = [...Array(rows * columns).keys()];
+const getShuffledArray = (arr: (number | null)[]) => {
+  const result = [...arr];
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+  return result;
+};
 
-    for (let i = numbers.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [numbers[i], numbers[j]] = [numbers[j], numbers[i]];
+const values = getShuffledArray([
+  null,
+  ...Array(rows * columns - 1)
+    .fill(null)
+    .map((_, i) => i + 1),
+]);
+
+const inclusiveRange = (a: number, b: number) => {
+  const min = Math.min(a, b);
+  const max = Math.max(a, b);
+
+  const list = [];
+  for (let i = min; i <= max; i++) {
+    list.push(i);
+  }
+
+  if (a > b) {
+    list.reverse();
+  }
+
+  return list;
+};
+
+const getPositionsBetween = (pos1: number[], pos2: number[]) => {
+  const [i1, j1] = pos1;
+  const [i2, j2] = pos2;
+
+  if (i1 === i2) {
+    const result = [];
+    for (const j of inclusiveRange(j1, j2)) {
+      result.push([i1, j]);
     }
-    setTiles(numbers);
+    return result;
+  }
+
+  if (j1 === j2) {
+    const result = [];
+    for (const i of inclusiveRange(i1, i2)) {
+      result.push([i, j1]);
+    }
+    return result;
+  }
+
+  return null;
+};
+
+export const Puzzle = () => {
+  const [board, setBoard] = useState(() =>
+    Array(rows)
+      .fill(null)
+      .map((_, i) =>
+        Array(columns)
+          .fill(null)
+          .map((_, j) => values[i * columns + j])
+      )
+  );
+
+  const setPos = (pos: number[], val: number | null) => {
+    setBoard((prevBoard) => {
+      const newBoard = [...prevBoard];
+      const [i, j] = pos;
+      newBoard[i][j] = val;
+      return newBoard;
+    });
   };
 
-  useEffect(() => {
-    shuffle();
-  }, []);
+  const getPosOfNull = () => {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        if (board[i][j] === null) {
+          return [i, j];
+        }
+      }
+    }
+  };
+
+  const getValue = (pos: number[]) => board[pos[0]][pos[1]];
+
+  const moveClickedTilesTowardNullPosition = (pos: number[]) => {
+    const nullPos = getPosOfNull();
+    const positionsBetween = getPositionsBetween(pos, nullPos);
+
+    if (positionsBetween === null) {
+      return false;
+    }
+
+    const valuesToMove = positionsBetween.map(getValue);
+
+    for (let i = 1; i < positionsBetween.length; i++) {
+      setPos(positionsBetween[i], valuesToMove[i - 1]);
+    }
+
+    setPos(positionsBetween[0], null);
+
+    return true;
+  };
+
+  const shuffleBoard = () => {
+    const newValues = getShuffledArray([
+      null,
+      ...Array(rows * columns - 1)
+        .fill(null)
+        .map((_, i) => i + 1),
+    ]);
+
+    const newBoard = Array(rows)
+      .fill(null)
+      .map((_, i) =>
+        Array(columns)
+          .fill(null)
+          .map((_, j) => newValues[i * columns + j])
+      );
+
+    setBoard(newBoard);
+  };
 
   return (
-    <div>
-      <PuzzleWrapper columns={columns} rows={rows}>
-        {tiles.map((number) => (
-          <Tile number={number} />
-        ))}
-      </PuzzleWrapper>
-      <button onClick={shuffle}>Shuffle</button>
-    </div>
+    <PuzzleWrapper>
+      <h1>N-puzzle</h1>
+      <StyledPuzzle columns={columns} rows={rows}>
+        {board.map((row, i) => {
+          return row.map((value, j) => {
+            return (
+              <Tile
+                value={value}
+                key={`${i}-${j}`}
+                onMove={() => moveClickedTilesTowardNullPosition([i, j])}
+              />
+            );
+          });
+        })}
+      </StyledPuzzle>
+      <Button onClick={shuffleBoard}>Shuffle</Button>
+    </PuzzleWrapper>
   );
 };
--- src/Tile.tsx
+++ src/Tile.tsx
@@ -1,13 +1,40 @@
 import styled from "styled-components";
 
 type TileProps = {
-  number: number;
+  value: number | null;
+  onMove: () => void;
 };
 
-const TileWrapper = styled.div`
-  border: 1px solid black;
+const TileWrapper = styled.div<{ isClickable: boolean }>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 0.5px solid grey;
+  cursor: ${({ isClickable }) => (isClickable ? "pointer" : "default")};
+  opacity: ${({ isClickable }) => (isClickable ? 1 : 0.5)};
+  box-shadow: 0 0.5rem 1rem lightgray;
+  border-radius: 0.5rem;
+  padding: 0.5rem;
 `;
 
-export const Tile = ({ number }: TileProps) => {
-  return <TileWrapper>{number}</TileWrapper>;
+export const Tile = ({ value, onMove }: TileProps) => {
+  const handleClick = () => {
+    if (value !== null) {
+      onMove();
+    }
+  };
+
+  const isClickable = value !== null;
+
+  return (
+    <>
+      {isClickable ? (
+        <TileWrapper isClickable={isClickable} onClick={handleClick}>
+          {value}
+        </TileWrapper>
+      ) : (
+        <TileWrapper isClickable={isClickable}>{value}</TileWrapper>
+      )}
+    </>
+  );
 };
